7f6be3c6c1363c482edbea067818d943930228c5
Tobias Bayer <tobayer@googlemail.com>
2013-07-14 14:46:05 +0200

    Add tests for buttons

--- test/clj/reindeer/t_core.clj
+++ test/clj/reindeer/t_core.clj
@@ -1,7 +1,22 @@
 (ns clj.reindeer.t_core
   (:use [clj.reindeer.core]
-        [midje.sweet]))
+        [midje.sweet])
+  (:import [com.vaadin.ui
+            Button$ClickEvent]))
+
+(defn on-click [e]
+  nil)
 
 (facts "about labels"
-  (let [l (label "Caption")]
-    (.getCaption l) => "Caption"))
+  (fact "it has the right caption"
+    (let [l (label "Caption")]
+      (.getCaption l) => "Caption")))
+
+(facts "about buttons"
+  (let [b (button :caption "Caption" :on-click on-click)]
+    (fact "it has the right caption"
+      (.getCaption b) => "Caption")
+    (fact "it calls the listener function"
+      (.click b) => irrelevant 
+        (provided
+          (on-click anything) => irrelevant :times 1))))
